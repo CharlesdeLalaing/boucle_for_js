7426590a85b83c2bcaf0c0535c7ef9aa215c390f
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-21 14:09:56 +0200

    push

---
+++ main.js
@@ -0,0 +1,20 @@
+// -  # Exo 1 Boucles FOR
+//     ## - Creer une varaible de type array et inserez toutes les personnes de la classe
+//     ## - A l'aide d'une boucle for, dites `bonjour + prenom` à chaque personne du tableau
+
+
+// let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+// for(let i = 0; i < eleves.length; i++) {
+//     console.log("Bonjour " + eleves[i]);
+// }
+
+// - # Exo 2 Boucles FOR (let)
+//     ## - Dans un prompt inserés un nombre 
+//     ## - A l'aide d'une boucle for, affichez les nombres de 0 jusqu'au nombre que vous avez choisi.
+
+// let chiffre = prompt("choississez un chiffre");
+
+// for(let i = 0; i <= chiffre; i++) {
+//     console.log(i);
+// }
